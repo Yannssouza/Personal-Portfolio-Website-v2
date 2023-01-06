9561b06d4eadd8c8ddbe920cabe5163fb2528abf
Yannssouza <115546740+Yannssouza@users.noreply.github.com>
2023-01-06 11:55:20 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main";
+import "../font/inter.css";
+import "./index.css";
+
+function App() {
+  return (
+    <Main />
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: "class",
+  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ['Staatliches', 'cursive']
+      }
+    },
+  },
+  plugins: [],
+}
