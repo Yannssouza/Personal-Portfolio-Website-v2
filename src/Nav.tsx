d2c412e3b32bf93c34d2dba68ce936f566441776
Yann <yannssouza@outlook.com>
2023-01-11 07:17:29 -0300

    update

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -17,7 +17,7 @@ function Nav(props: {
           <li>
               <span className="relative cursor-pointer" onClick={props.changeMode}>
                 <span className="block w-10 h-6 bg-gray-400 rounded-full shadow-inner"></span>
-                <span className="flex justify-center items-center absolute block w-4 h-4 mt-1 ml-1 bg-white rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transition-transform duration-300 ease-in-out dark:transition-transform dark:duration-300 dark:ease-in-out dark:bg-white dark:transform dark:translate-x-full">
+                <span className="flex justify-center items-center absolute w-4 h-4 mt-1 ml-1 bg-white rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transition-transform duration-300 ease-in-out dark:transition-transform dark:duration-300 dark:ease-in-out dark:bg-white dark:transform dark:translate-x-full">
                   {props.darkMode ? <i className="fa-solid fa-sun fa-2xs text-cyan-600 dark:text-amber-400"></i> : <i className="fa-solid fa-moon fa-2xs text-cyan-600 dark:text-amber-400"></i>}
                 </span>
               </span>
